678212b8036311a7f34de27b3e94d48047762d8b
ikhsolidum <100141624+ikhsolidum@users.noreply.github.com>
2023-02-15 12:30:24 +0800

    Add files via upload

---
+++ WebDevApp/orders.php
@@ -0,0 +1,32 @@
+<?php
+// Connect to the database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test_db";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Retrieve the form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$productid = $_POST['productid'];
+$quantity = $_POST['quantity'];
+
+// Insert the form data into the database
+$sql = "INSERT INTO orders (name, email, productid, quantity) VALUES ('$name', '$email', '$productid', '$quantity')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+// Close the database connection
+mysqli_close($conn);
+?>
